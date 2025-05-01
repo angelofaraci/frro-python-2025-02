f6dfefe74d2022d38ea12dcc2404c673a30b6fce
unknown <nahuelverdera1@hotmail.com>
2025-05-01 20:12:56 -0300

    Ejemplo stencil funcional

--- stencil_view_example2/example2.py
+++ stencil_view_example2/example2.py
@@ -1,44 +1,85 @@
+'''
+Stencil demo
+============
+
+This is a test of the stencil graphics instruction inside the stencil view
+widget. When you use a stencil, nothing will be drawn outside the bounding
+box. All the graphics will draw only in the stencil view.
+
+You can "draw" a stencil view by touch & draw. The touch down will set the
+position, and the drag will set the size.
+'''
+
 from kivy.app import App
-from kivy.uix.stencilview import StencilView
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.image import Image
-from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
+from kivy.graphics import Color, Rectangle
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.stencilview import StencilView
+from random import random as r
+from functools import partial
 
-class MyGallery(StencilView):
-    pass
 
-class GalleryApp(App):
+class StencilTestWidget(StencilView):
+    '''Drag to define stencil area
+    '''
+
+    def on_touch_down(self, touch):
+        self.pos = touch.pos
+        self.size = (1, 1)
+
+    def on_touch_move(self, touch):
+        self.size = (touch.x - touch.ox, touch.y - touch.oy)
+
+
+class StencilCanvasApp(App):
+
+    def add_rects(self, label, wid, count, *largs):
+        label.text = str(int(label.text) + count)
+        with wid.canvas:
+            for x in range(count):
+                Color(r(), 1, 1, mode='hsv')
+                Rectangle(pos=(r() * wid.width + wid.x,
+                               r() * wid.height + wid.y), size=(10, 10))
+
+    def reset_stencil(self, wid, *largs):
+        wid.pos = (0, 0)
+        wid.size = Window.size
+
+    def reset_rects(self, label, wid, *largs):
+        label.text = '0'
+        wid.canvas.clear()
+
     def build(self):
-        Window.size = (800, 600)
+        wid = StencilTestWidget(size_hint=(None, None), size=Window.size)
 
-        root = BoxLayout(orientation='vertical', spacing=10, padding=10)
+        label = Label(text='0')
 
-        # Outer container (StencilView)
-        stencil = MyGallery(size_hint=(1, 1))
+        btn_add500 = Button(text='+ 200 rects')
+        btn_add500.bind(on_press=partial(self.add_rects, label, wid, 200))
 
-        # ScrollView with fixed height
-        scroll = ScrollView(size_hint=(1, None), height=220, do_scroll_y=False)
+        btn_reset = Button(text='Reset Rectangles')
+        btn_reset.bind(on_press=partial(self.reset_rects, label, wid))
 
-        # Inner layout for images
-        inner = BoxLayout(orientation='horizontal', spacing=10, size_hint=(None, None), height=200)
-        inner.bind(minimum_width=inner.setter('width'))
+        btn_stencil = Button(text='Reset Stencil')
+        btn_stencil.bind(on_press=partial(self.reset_stencil, wid))
 
-        # Add images
-        for i in range(5):
-            img = Image(
-                source=f"../stencil_view_example/example_image{i}.jpg",
-                size_hint=(None, None),
-                size=(200, 200),
-                allow_stretch=True,
-                keep_ratio=True
-            )
-            inner.add_widget(img)
+        layout = BoxLayout(size_hint=(1, None), height=50)
+        layout.add_widget(btn_add500)
+        layout.add_widget(btn_reset)
+        layout.add_widget(btn_stencil)
+        layout.add_widget(label)
 
-        scroll.add_widget(inner)
-        stencil.add_widget(scroll)
-        root.add_widget(stencil)
+        root = BoxLayout(orientation='vertical')
+        rfl = FloatLayout()
+        rfl.add_widget(wid)
+        root.add_widget(rfl)
+        root.add_widget(layout)
 
         return root
 
-GalleryApp().run()
+
+if __name__ == '__main__':
+    StencilCanvasApp().run()
